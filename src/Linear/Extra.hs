ed73b4ada0f54c219e5272c622fdf0747e7a36d9
Luke Iannini <lukexi@me.com>
2015-09-21 02:41:52 -0700

    Add interpolatePoses

--- src/Linear/Extra.hs
+++ src/Linear/Extra.hs
@@ -1,5 +1,6 @@
 module Linear.Extra
-  ( module Exports
+  ( module Linear.Extra
+  , module Exports
   ) where
 
 import Linear                             as Exports
@@ -12,3 +13,8 @@ scaleMatrix (V3 x y z) = V4 (V4 x 0 0 0)
                             (V4 0 y 0 0)
                             (V4 0 0 z 0)
                             (V4 0 0 0 1)
+
+
+interpolatePoses :: (Num a, Fractional a, RealFloat a) => Pose a -> Pose a -> Pose a
+interpolatePoses (Pose p1 o1) (Pose p2 o2) =
+  Pose (lerp 0.5 p1 p2) (slerp o1 o2 0.5)
