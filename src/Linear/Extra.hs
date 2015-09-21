426aaa7d13dec4739fc2424393c1e707898ad274
Luke Iannini <lukexi@me.com>
2015-09-21 01:44:58 -0700

    Add scaleMatrix

--- src/Linear/Extra.hs
+++ src/Linear/Extra.hs
@@ -2,6 +2,13 @@ module Linear.Extra
   ( module Exports
   ) where
 
-import Linear as Exports
+import Linear                             as Exports
 import Linear.Extra.Pose                  as Exports
 import Linear.Extra.CompressedQuaternion  as Exports
+
+
+scaleMatrix :: (Num a) => V3 a -> M44 a
+scaleMatrix (V3 x y z) = V4 (V4 x 0 0 0)
+                            (V4 0 y 0 0)
+                            (V4 0 0 z 0)
+                            (V4 0 0 0 1)
