3d2c04186c6404a4e587ed8c3fc12efe45e25708
Luke Iannini <lukexi@me.com>
2016-08-17 18:14:23 -0700

    Expose affine

--- src/Linear/Extra.hs
+++ src/Linear/Extra.hs
@@ -4,6 +4,7 @@ module Linear.Extra
   ) where
 
 import Linear                             as Exports
+import Linear.Affine                      as Exports
 import Linear.Extra.Pose                  as Exports
 import Linear.Extra.Ray                   as Exports
 import Linear.Extra.Matrix                as Exports
